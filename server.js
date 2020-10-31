ab117c31528bdfef60cd187953eee272713625b2
David Guthmann <drguthmann@cox.net>
2020-10-31 15:12:44 -0500

    Formatted ReadMe, added controller routes to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const exphbs = require("express-handlebars")
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,8 +18,17 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
 
-// Create Routes Here
-// get route for all
+//set handlebars as default template
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Import Routes for controllers
+// For handling db requests
+const workoutController = require('./controllers/workout-controller')
+app.use(workoutController)
+// For handling page rendering
+const htmlController = require('./controllers/html-controller')
+app.use(htmlController)
 
 // Start the server
 app.listen(PORT, () => {
